8be5e62519e3f51133cbc377ddb3911e601579e0
Kohei Yoshida <kohei.yoshida@gmail.com>
2016-12-17 09:39:29 -0500

    Test to make sure we are picking up merged cell ranges correctly.

--- src/orcus_test_xls_xml.cpp
+++ src/orcus_test_xls_xml.cpp
@@ -11,6 +11,7 @@
 #include "orcus/stream.hpp"
 #include "orcus/spreadsheet/factory.hpp"
 #include "orcus/spreadsheet/document.hpp"
+#include "orcus/spreadsheet/sheet.hpp"
 
 #include <cassert>
 #include <string>
@@ -59,11 +60,55 @@ void test_xls_xml_import()
     }
 }
 
+void test_xls_xml_merged_cells()
+{
+    const char* filepath = SRCDIR"/test/xls-xml/merged-cells/input.xml";
+
+    spreadsheet::document doc;
+    spreadsheet::import_factory factory(doc);
+    orcus_xls_xml app(&factory);
+    app.read_file(filepath);
+
+    const spreadsheet::sheet* sheet1 = doc.get_sheet("Sheet1");
+    assert(sheet1);
+
+    spreadsheet::range_t merge_range = sheet1->get_merge_cell_range(0, 1);
+    assert(merge_range.first.column == 1);
+    assert(merge_range.last.column == 2);
+    assert(merge_range.first.row == 0);
+    assert(merge_range.last.row == 0);
+
+    merge_range = sheet1->get_merge_cell_range(0, 3);
+    assert(merge_range.first.column == 3);
+    assert(merge_range.last.column == 5);
+    assert(merge_range.first.row == 0);
+    assert(merge_range.last.row == 0);
+
+    merge_range = sheet1->get_merge_cell_range(1, 0);
+    assert(merge_range.first.column == 0);
+    assert(merge_range.last.column == 0);
+    assert(merge_range.first.row == 1);
+    assert(merge_range.last.row == 2);
+
+    merge_range = sheet1->get_merge_cell_range(3, 0);
+    assert(merge_range.first.column == 0);
+    assert(merge_range.last.column == 0);
+    assert(merge_range.first.row == 3);
+    assert(merge_range.last.row == 5);
+
+    merge_range = sheet1->get_merge_cell_range(2, 2);
+    assert(merge_range.first.column == 2);
+    assert(merge_range.last.column == 5);
+    assert(merge_range.first.row == 2);
+    assert(merge_range.last.row == 5);
+}
+
 }
 
 int main()
 {
     test_xls_xml_import();
+    test_xls_xml_merged_cells();
     return EXIT_SUCCESS;
 }
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
