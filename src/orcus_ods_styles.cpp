dc9365eb070b5120d8b9ac688d25bff28e64dc99
Markus Mohrhard <markus.mohrhard@googlemail.com>
2017-01-06 00:55:30 +0100

    add app for fuzzing the odf styles import

---
+++ src/orcus_ods_styles.cpp
@@ -0,0 +1,40 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "orcus/orcus_import_ods.hpp"
+#include "orcus/spreadsheet/styles.hpp"
+#include "orcus/spreadsheet/factory.hpp"
+#include "orcus/string_pool.hpp"
+#include "orcus/stream.hpp"
+
+#include "orcus_filter_global.hpp"
+
+#include <cstdlib>
+
+using namespace std;
+using namespace orcus;
+
+int main(int argc, char** argv)
+{
+    if (argc != 2)
+        return EXIT_FAILURE;
+    string_pool sp;
+    spreadsheet::import_styles style(sp);
+
+    std::string content = load_file_content(argv[1]);
+    try
+    {
+        import_ods::read_styles(content.c_str(), content.length(), &style);
+    }
+    catch(...)
+    {
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
