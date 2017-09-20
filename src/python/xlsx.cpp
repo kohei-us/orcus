edeb521f28b2d56e96286c8f28810c24ff806283
Kohei Yoshida <kohei.yoshida@gmail.com>
2017-09-19 21:53:50 -0400

    python: first cut on xlsx.read().
    Some clean up to do later, as some temporary copy-n-paste has been
    done...

--- src/python/xlsx.cpp
+++ src/python/xlsx.cpp
@@ -56,9 +56,56 @@ PyObject* xlsx_read_file(PyObject* /*module*/, PyObject* args, PyObject* kwargs)
 
 PyObject* xlsx_read(PyObject* /*module*/, PyObject* args, PyObject* kwargs)
 {
-    // TODO : implement this.
-    Py_INCREF(Py_None);
-    return Py_None;
+    static const char* kwlist[] = { "file", nullptr };
+
+    PyObject* file = nullptr;
+
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "O", const_cast<char**>(kwlist), &file))
+        return nullptr;
+
+    if (!file)
+    {
+        PyErr_SetString(PyExc_RuntimeError, "Invalid file object has been passed.");
+        return nullptr;
+    }
+
+    PyObject* func_read = PyObject_GetAttrString(file, "read");
+    if (!func_read)
+    {
+        PyErr_SetString(PyExc_RuntimeError, "'read' function was expected, but not found.");
+        return nullptr;
+    }
+
+    PyObject* obj_bytes = PyObject_CallFunction(func_read, nullptr);
+    if (!obj_bytes)
+    {
+        PyErr_SetString(PyExc_RuntimeError, "The read function didn't return bytes.");
+        return nullptr;
+    }
+
+    const char* p = PyBytes_AS_STRING(obj_bytes);
+    size_t n = PyBytes_Size(obj_bytes);
+
+    std::unique_ptr<spreadsheet::document> doc = orcus::make_unique<spreadsheet::document>();
+    spreadsheet::import_factory fact(*doc);
+    orcus_xlsx app(&fact);
+
+    app.read_stream(p, n);
+
+    PyTypeObject* doc_type = get_document_type();
+    if (!doc_type)
+        return nullptr;
+
+    PyObject* obj_doc = doc_type->tp_new(doc_type, nullptr, nullptr);
+    if (!obj_doc)
+        return nullptr;
+
+    doc_type->tp_init(obj_doc, nullptr, nullptr);
+
+    store_document(obj_doc, std::move(doc));
+
+    Py_INCREF(obj_doc);
+    return obj_doc;
 }
 
 #else
