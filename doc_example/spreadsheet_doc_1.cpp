a67dd7e709476098c9cbf67a170290c1cd9eca56
Kohei Yoshida <kohei.yoshida@gmail.com>
2018-02-03 16:34:20 -0500

    Finish the example code.

--- doc_example/spreadsheet_doc_1.cpp
+++ doc_example/spreadsheet_doc_1.cpp
@@ -1,18 +1,47 @@
 
 #include <orcus/spreadsheet/document.hpp>
 #include <orcus/spreadsheet/factory.hpp>
-#include <orcus/orcus_xlsx.hpp>
+#include <orcus/orcus_ods.hpp>
+
+#include <ixion/model_context.hpp>
+#include <iostream>
 
 using namespace orcus;
 
 int main()
 {
+    // Instantiate a document, and wrap it with a factory.
     spreadsheet::document doc;
     spreadsheet::import_factory factory(doc);
 
-    orcus_xlsx loader(&factory);
+    // Pass the factory to the document loader, and read the content from a file
+    // to populate the document.
+    orcus_ods loader(&factory);
+    loader.read_file(SRCDIR"/doc_example/files/document.ods");
 
-    // TODO : continue on...
+    // Now that the document is fully populated, access its content.
+    const ixion::model_context& model = doc.get_model_context();
+
+    // Read the header row and print its content.
+
+    ixion::abs_address_t pos(0, 0, 0); // Set the cell position to A1.
+    ixion::string_id_t str_id = model.get_string_identifier(pos);
+
+    const std::string* s = model.get_string(str_id);
+    assert(s);
+    std::cout << "A1: " << *s << std::endl;
+
+    pos.column = 1; // Move to B1
+    str_id = model.get_string_identifier(pos);
+    s = model.get_string(str_id);
+    assert(s);
+    std::cout << "B1: " << *s << std::endl;
+
+    pos.column = 2; // Move to C1
+    str_id = model.get_string_identifier(pos);
+    s = model.get_string(str_id);
+    assert(s);
+    std::cout << "C1: " << *s << std::endl;
 
     return EXIT_SUCCESS;
 }
