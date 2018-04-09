1123c5b1e67564ca1f3942fee691c79215839539
Kohei Yoshida <kohei.yoshida@gmail.com>
2018-04-08 20:49:46 -0400

    Add formula array test case to the automated test.

--- src/orcus_test_xlsx.cpp
+++ src/orcus_test_xlsx.cpp
@@ -80,6 +80,7 @@ vector<const char*> dirs = {
     SRCDIR"/test/xlsx/raw-values-1/",
     SRCDIR"/test/xlsx/boolean-values/",
     SRCDIR"/test/xlsx/empty-shared-strings/",
+    SRCDIR"/test/xlsx/formula-array-1/",
     SRCDIR"/test/xlsx/formula-cells/",
     SRCDIR"/test/xlsx/formula-shared/",
     SRCDIR"/test/xlsx/named-expression/",
