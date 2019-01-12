2d8bbda2507249eac4755a1c075338b3b8704802
Kohei Yoshida <kohei.yoshida@gmail.com>
2019-01-12 16:15:55 -0500

    Use boost::object_pool in the implementation of string_pool.
    This addresses #69.  Unfortunately this change doesn't give us
    noticeable performance improvement I was hoping to see.  But it
    at least keeps the implementation a bit cleaner.

--- src/parser/string_pool.cpp
+++ src/parser/string_pool.cpp
@@ -18,49 +18,27 @@
 #include <cassert>
 #include <algorithm>
 
-using namespace std;
-
-namespace {
-
-struct dump_instance : public unary_function<string*, void>
-{
-    dump_instance() : counter(0) {}
-    void operator() (const string* p)
-    {
-        cout << counter++ << ": '" << *p << "'" << endl;
-    }
-private:
-    size_t counter;
-};
-
-class pstring_back_inserter
-{
-    vector<const string*>& m_store;
-public:
-    pstring_back_inserter(vector<const string*>& store) : m_store(store) {}
-    void operator() (const unique_ptr<string>& p) { m_store.push_back(p.get()); }
-};
-
-struct pstring_less
-{
-    bool operator() (const string* p1, const string* p2) const { return m_less(*p1, *p2); }
-private:
-    less<string> m_less;
-};
-
-}
+#include <boost/pool/object_pool.hpp>
 
 namespace orcus {
 
-typedef std::unordered_set<pstring, pstring::hash> string_set_type;
-typedef std::vector<std::unique_ptr<std::string>> string_store_type;
+using std::cout;
+using std::endl;
+
+using string_set_type = std::unordered_set<pstring, pstring::hash>;
+using string_store_type = boost::object_pool<std::string>;
+using string_stores_type = std::vector<std::unique_ptr<string_store_type>>;
 
 struct string_pool::impl
 {
+    string_stores_type m_stores;
     string_set_type m_set;
-    string_store_type m_store;
-    /** duplicate string instances */
-    string_store_type m_merged_store;
+
+    impl()
+    {
+        // first element is the active store used for the current instance.
+        m_stores.push_back(orcus::make_unique<string_store_type>());
+    }
 };
 
 string_pool::string_pool() : mp_impl(orcus::make_unique<impl>()) {}
@@ -70,60 +48,71 @@ string_pool::~string_pool()
     clear();
 }
 
-pair<pstring, bool> string_pool::intern(const char* str)
+std::pair<pstring, bool> string_pool::intern(const char* str)
 {
     return intern(str, strlen(str));
 }
 
-pair<pstring, bool> string_pool::intern(const char* str, size_t n)
+std::pair<pstring, bool> string_pool::intern(const char* str, size_t n)
 {
     if (!n)
-        return pair<pstring, bool>(pstring(), false);
+        return std::pair<pstring, bool>(pstring(), false);
 
     string_set_type::const_iterator itr = mp_impl->m_set.find(pstring(str, n));
     if (itr == mp_impl->m_set.end())
     {
         // This string has not been interned.  Intern it.
-        mp_impl->m_store.push_back(orcus::make_unique<string>(str, n));
-        pair<string_set_type::iterator,bool> r = mp_impl->m_set.insert(pstring(mp_impl->m_store.back()->data(), n));
+        string_store_type& store = *mp_impl->m_stores[0];
+        std::string* p = store.construct(str, n);
+        if (!p)
+            throw general_error("failed to intern a new string instance.");
+
+        std::pair<string_set_type::iterator,bool> r =
+            mp_impl->m_set.emplace(p->data(), p->size());
         if (!r.second)
             throw general_error("failed to intern a new string instance.");
+
         const pstring& ps = *r.first;
         assert(ps.size() == n);
 
-        return pair<pstring, bool>(ps, true);
+        return std::pair<pstring, bool>(ps, true);
     }
 
     // This string has already been interned.
 
     const pstring& stored_str = *itr;
     assert(stored_str.size() == n);
-    return pair<pstring, bool>(stored_str, false);
+    return std::pair<pstring, bool>(stored_str, false);
 }
 
-pair<pstring, bool> string_pool::intern(const pstring& str)
+std::pair<pstring, bool> string_pool::intern(const pstring& str)
 {
     return intern(str.get(), str.size());
 }
 
 void string_pool::dump() const
 {
-    cout << "interned string count: " << mp_impl->m_store.size() << endl;
+    cout << "interned string count: " << mp_impl->m_set.size() << endl;
 
     // Sort stored strings first.
-    vector<const string*> sorted;
-    sorted.reserve(mp_impl->m_store.size());
-    for_each(mp_impl->m_store.begin(), mp_impl->m_store.end(), pstring_back_inserter(sorted));
-    sort(sorted.begin(), sorted.end(), pstring_less());
+    std::vector<pstring> sorted;
+    sorted.reserve(mp_impl->m_set.size());
+
+    for (const pstring& ps : mp_impl->m_set)
+        sorted.push_back(ps);
+
+    sort(sorted.begin(), sorted.end());
 
     // Now dump them all to stdout.
-    for_each(sorted.begin(), sorted.end(), dump_instance());
+    size_t counter = 0;
+    for (const pstring& s : sorted)
+        cout << counter++ << ": '" << s << "'" << endl;
 }
 
 void string_pool::clear()
 {
     mp_impl->m_set.clear();
-    mp_impl->m_store.clear();
+    mp_impl->m_stores.clear();
 }
 
 size_t string_pool::size() const
@@ -138,43 +127,16 @@ void string_pool::swap(string_pool& other)
 
 void string_pool::merge(string_pool& other)
 {
-    string_store_type* other_store = &other.mp_impl->m_store;
-
-    for (string_store_type::value_type& value : *other_store)
+    while (!other.mp_impl->m_stores.empty())
     {
-        const std::string* p = value.get();
-        size_t n = p->size();
-
-        pstring key(p->data(), n);
-        string_set_type::const_iterator it = mp_impl->m_set.find(key);
-
-        if (it == mp_impl->m_set.end())
-        {
-            // This is a new string value in this pool.  Move this string
-            // instance in as-is.
-            mp_impl->m_store.push_back(std::move(value));
-            assert(key.get() == mp_impl->m_store.back()->data());
-            auto r = mp_impl->m_set.insert(key);
-            if (!r.second)
-                throw general_error("failed to intern a new string instance.");
-        }
-        else
-        {
-            // This is a duplicate string value in this pool.  Move this
-            // string instance in to the merged store.
-            mp_impl->m_merged_store.push_back(std::move(value));
-        }
+        mp_impl->m_stores.push_back(
+            std::move(other.mp_impl->m_stores.back()));
+        other.mp_impl->m_stores.pop_back();
     }
 
-    // Move all duplicate string values from the other store as-is.
-    other_store = &other.mp_impl->m_merged_store;
-    for (string_store_type::value_type& value : *other_store)
-    {
-        mp_impl->m_merged_store.push_back(std::move(value));
-    }
+    for (const pstring& p : other.mp_impl->m_set)
+        mp_impl->m_set.insert(p);
 
-    other.mp_impl->m_store.clear();
-    other.mp_impl->m_merged_store.clear();
     other.mp_impl->m_set.clear();
 }
 
