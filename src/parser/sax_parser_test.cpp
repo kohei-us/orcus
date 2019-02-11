96edfa8b460f1ed3af4779eed01e68b5c20a1613
Kohei Yoshida <kohei.yoshida@gmail.com>
2019-02-11 18:31:07 -0500

    Add test case for transient stream handling in sax_parser.

---
+++ src/parser/sax_parser_test.cpp
@@ -0,0 +1,91 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "orcus/sax_parser.hpp"
+#include <cassert>
+#include <iostream>
+
+using namespace std;
+
+void test_transient_stream()
+{
+    struct _handler
+    {
+        void doctype(const orcus::sax::doctype_declaration& param) {}
+
+        void start_declaration(const orcus::pstring& decl) {}
+
+        void end_declaration(const orcus::pstring& decl) {}
+
+        void start_element(const orcus::sax::parser_element& elem) {}
+
+        void end_element(const orcus::sax::parser_element& elem) {}
+
+        void characters(const orcus::pstring& val, bool transient)
+        {
+            cout << "characters: '" << val << "' (transient=" << transient << ")" << endl;
+
+            if (transient_stream)
+                // When parsing a transient stream, this flag is always set.
+                assert(transient);
+            else if (val == "non-transient")
+                assert(!transient);
+            else if (val == "(&&&)")
+                assert(transient);
+            else if (val == "    ")
+                assert(!transient);
+        }
+
+        void attribute(const orcus::sax::parser_attribute& attr)
+        {
+            cout << "attribute: " << attr.name << "=\"" << attr.value << "\" (transient=" << attr.transient << ")" << endl;
+
+            if (transient_stream)
+                // When parsing a transient stream, this flag is always set.
+                assert(attr.transient);
+            else if (attr.name == "attr1")
+                assert(!attr.transient);
+            else if (attr.name == "attr2")
+                assert(attr.transient);
+            else if (attr.name == "version")
+                assert(!attr.transient);
+        }
+
+        bool transient_stream = false;
+    };
+
+    const char* content =
+        "<?xml version=\"1.0\"?>"
+        "<root attr1=\"non-transient\" attr2=\"&amp; transient\">"
+        "    <content1>non-transient</content1>"
+        "    <content2>(&amp;&amp;&amp;)</content2>"
+        "</root>"
+    ;
+
+    {
+        _handler hdl;
+        hdl.transient_stream = false;
+        orcus::sax_parser<_handler> parser(content, strlen(content), hdl.transient_stream, hdl);
+        parser.parse();
+    }
+
+    {
+        _handler hdl;
+        hdl.transient_stream = true;
+        orcus::sax_parser<_handler> parser(content, strlen(content), hdl.transient_stream, hdl);
+        parser.parse();
+    }
+}
+
+int main()
+{
+    test_transient_stream();
+
+    return EXIT_SUCCESS;
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
