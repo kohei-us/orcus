45eaf06c7c28a4668ec338221d43c6c0cf3d12ea
Kohei Yoshida <kohei.yoshida@gmail.com>
2019-06-22 17:43:07 -0400

    Generate expected outputs.

---
+++ bin/gen-test-json-structure-outputs.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+PROGDIR=`dirname $0`
+TESTDIR="$PROGDIR/../test/json-structure"
+ORCUS_JSON_EXEC="$PROGDIR/../src/orcus-json"
+
+for _DIR in $(ls $TESTDIR); do
+    echo "processing $_DIR..."
+    _DIR="$TESTDIR/$_DIR"
+    _INPUT="$_DIR/input.json"
+    _OUTPUT="$_DIR/output.txt"
+    $ORCUS_JSON_EXEC --mode structure -o "$_OUTPUT" "$_INPUT"
+done
+
