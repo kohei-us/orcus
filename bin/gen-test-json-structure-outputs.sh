0abaa6858c6be983c1c9f4abf3292160886e67be
Kohei Yoshida <kohei.yoshida@gmail.com>
2019-06-22 17:53:14 -0400

    Expected output file should be named check.txt.

--- bin/gen-test-json-structure-outputs.sh
+++ bin/gen-test-json-structure-outputs.sh
@@ -8,7 +8,7 @@ for _DIR in $(ls $TESTDIR); do
     echo "processing $_DIR..."
     _DIR="$TESTDIR/$_DIR"
     _INPUT="$_DIR/input.json"
-    _OUTPUT="$_DIR/output.txt"
+    _OUTPUT="$_DIR/check.txt"
     $ORCUS_JSON_EXEC --mode structure -o "$_OUTPUT" "$_INPUT"
 done
 
