640e6371d0ad131706dbc2574726de98cec874f4
Kohei Yoshida <kohei.yoshida@gmail.com>
2019-06-27 09:29:55 -0400

    Start parsing the map file.

--- src/orcus_json_main.cpp
+++ src/orcus_json_main.cpp
@@ -13,7 +13,6 @@
 #include "orcus/xml_namespace.hpp"
 #include "orcus/dom_tree.hpp"
 #include "orcus/global.hpp"
-#include "orcus/orcus_json.hpp"
 
 #include <iostream>
 #include <fstream>
@@ -335,6 +334,61 @@ void build_doc_and_dump(std::ostream& os, const orcus::file_content& content, co
     }
 }
 
+void map_to_sheets_and_dump(std::ostream& os, const orcus::file_content& content, const cmd_params& params)
+{
+    cout << "TODO: implement this." << endl;
+    cout << params.map_file.data() << endl;
+
+    // Since a typical map file will likely be very small, let's be lazy and
+    // load the whole thing into a in-memory tree.
+    json::document_tree map_doc;
+    json_config jc;
+    jc.preserve_object_order = false;
+    jc.persistent_string_values = false;
+    jc.resolve_references = false;
+
+    map_doc.load(params.map_file.data(), params.map_file.size(), jc);
+    json::const_node root = map_doc.get_document_root();
+
+    {
+        // Create sheets first.
+
+        auto node = root.child("sheets");
+        for (size_t i = 0, n = node.child_count(); i < n; ++i)
+        {
+            auto node_name = node.child(i);
+            cout << "* sheet: " << node_name.string_value() << endl;
+        }
+    }
+
+    {
+        // Set cell links.
+        auto node = root.child("cells");
+        for (size_t i = 0, n = node.child_count(); i < n; ++i)
+        {
+            auto link_node = node.child(i);
+            cout << "* cell link: (path=" << link_node.child("path").string_value()
+                << "; sheet=" << link_node.child("sheet").string_value()
+                << "; row=" << link_node.child("row").numeric_value()
+                << "; column=" << link_node.child("column").numeric_value()
+                << ")" << endl;
+        }
+    }
+
+    {
+        // Set range links.
+        auto node = root.child("ranges");
+        for (size_t i = 0, n = node.child_count(); i < n; ++i)
+        {
+            auto link_node = node.child(i);
+            cout << "* range link: (sheet=" << link_node.child("sheet").string_value()
+                << "; row=" << link_node.child("row").numeric_value()
+                << "; column=" << link_node.child("column").numeric_value()
+                << ")" << endl;
+        }
+    }
+}
+
 } // anonymous namespace
 
 int main(int argc, char** argv)
@@ -371,9 +425,10 @@ int main(int argc, char** argv)
                 break;
             }
             case mode::type::map:
-                cout << "TODO: implement this." << endl;
-                cout << params.map_file.data() << endl;
+            {
+                map_to_sheets_and_dump(*os, content, params);
                 break;
+            }
             case mode::type::convert:
             {
                 build_doc_and_dump(*os, content, params);
