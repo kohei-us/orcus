79569b1e208c8bfb92b937e67336a366105dc0db
Kohei Yoshida <kohei.yoshida@gmail.com>
2019-07-02 14:14:34 -0400

    Handle appending of sheets.

--- src/liborcus/orcus_json.cpp
+++ src/liborcus/orcus_json.cpp
@@ -6,16 +6,25 @@
  */
 
 #include "orcus/orcus_json.hpp"
+#include "orcus/spreadsheet/import_interface.hpp"
+#include "orcus/global.hpp"
 #include "json_map_tree.hpp"
 
 namespace orcus {
 
 struct orcus_json::impl
 {
+    spreadsheet::iface::import_factory* im_factory;
+    spreadsheet::sheet_t sheet_count;
     json_map_tree map_tree;
+
+    impl(spreadsheet::iface::import_factory* _im_factory) :
+        im_factory(_im_factory), sheet_count(0) {}
 };
 
-orcus_json::orcus_json(spreadsheet::iface::import_factory* im_fact) {}
+orcus_json::orcus_json(spreadsheet::iface::import_factory* im_fact) :
+    mp_impl(orcus::make_unique<impl>(im_fact)) {}
+
 orcus_json::~orcus_json() {}
 
 void orcus_json::set_cell_link(const pstring& path, const pstring& sheet, spreadsheet::row_t row, spreadsheet::col_t col)
@@ -43,7 +52,13 @@ void orcus_json::commit_range()
     mp_impl->map_tree.commit_range();
 }
 
-void orcus_json::append_sheet(const pstring& name) {}
+void orcus_json::append_sheet(const pstring& name)
+{
+    if (name.empty())
+        return;
+
+    mp_impl->im_factory->append_sheet(mp_impl->sheet_count++, name.data(), name.size());
+}
 
 void orcus_json::read_stream(const char* p, size_t n) {}
 
