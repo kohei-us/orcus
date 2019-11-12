1074b47d72561ca6783142ddd5a46d938c97e611
Kohei Yoshida <kohei.yoshida@gmail.com>
2019-11-12 15:20:57 -0500

    Use thread pool to speed up the fetching of the attachments.

--- bin/download-files.py
+++ bin/download-files.py
@@ -7,10 +7,10 @@ import pprint
 import os
 import os.path
 import base64
+import concurrent.futures as cf
 
 
 BZURL = "bugs.documentfoundation.org"
-BZ_PARAMS = {"product": "LibreOffice", "component": "Calc"}
 CACHE_DIR = os.path.join(os.path.dirname(__file__), ".download-files")
 os.makedirs(CACHE_DIR, exist_ok=True)
 
@@ -27,19 +27,21 @@ def get_cache_content(cache_file, func_fetch):
     return s
 
 
-def get_bug_ids():
+def get_bug_ids(bz_params):
 
     def _fetch():
         r = requests.get(
             f"https://{BZURL}/rest/bug",
-            params=BZ_PARAMS
+            params=bz_params
         )
 
         if r.status_code != 200:
-            raise RuntimeError("failed to query bug ids from the TDF bugzilla!")
+            raise RuntimeError(f"failed to query bug ids from the TDF bugzilla! (status:{r.status_code})")
         return r.text
 
-    cache_file = os.path.join(CACHE_DIR, BZURL, "bug-ids.json")
+    limit = bz_params["limit"]
+    offset = bz_params["offset"]
+    cache_file = os.path.join(CACHE_DIR, BZURL, f"bug-ids-{limit}-{offset}.json")
     s = get_cache_content(cache_file, _fetch)
 
     content = json.loads(s)
@@ -57,7 +59,7 @@ def get_attachments(bug_id):
     def _fetch():
         r = requests.get(f"https://{BZURL}/rest/bug/{bug_id}/attachment")
         if r.status_code != 200:
-            raise RuntimeError(f"failed to fetch the attachments for bug {bug_id}!")
+            raise RuntimeError(f"failed to fetch the attachments for bug {bug_id}! (status:{r.status_code})")
         return r.text
 
     cache_file = os.path.join(CACHE_DIR, BZURL, f"attachments-{bug_id}.json")
@@ -78,19 +80,42 @@ def get_attachments(bug_id):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--outdir", "-o", type=str, required=True)
-    parser.add_argument("--limit", type=int, default=10)
+    parser.add_argument("--limit", type=int, default=500)
+    parser.add_argument("--offset", type=int, default=0)
+    parser.add_argument("--cont", action="store_true", default=False)
+    parser.add_argument("--worker", type=int, default=8)
     args = parser.parse_args()
-    BZ_PARAMS["limit"] = args.limit
+
+    bz_params = {"product": "LibreOffice", "component": "Calc"}
+    bz_params["limit"] = args.limit
+    bz_params["offset"] = args.offset
 
     os.makedirs(os.path.join(CACHE_DIR, BZURL), exist_ok=True)
-    for bug_id in get_bug_ids():
-        print(f"fetching attachments for bug {bug_id} ...", flush=True)
+
+    def _run(bug_id, index, totals):
+        print(f"({index+1}/{totals}) fetching attachments for bug {bug_id} ...", flush=True)
         attachments = get_attachments(bug_id)
         for attachment in attachments:
             filepath = os.path.join(args.outdir, str(bug_id), attachment["filename"])
             os.makedirs(os.path.dirname(filepath), exist_ok=True)
             with open(filepath, "wb") as f:
                 f.write(attachment["data"])
+
+    iter_count = 0
+    while True:
+        bug_ids = get_bug_ids(bz_params)
+        if not bug_ids:
+            return
+        print(f"-- iteration {iter_count+1}", flush=True)
+        with cf.ThreadPoolExecutor(max_workers=args.worker) as executor:
+            for i, bug_id in enumerate(bug_ids):
+                executor.submit(_run, bug_id, i, len(bug_ids))
+
+        if not args.cont:
+            return
+
+        bz_params["offset"] += bz_params["limit"]
+        iter_count += 1
 
 
 if __name__ == "__main__":
