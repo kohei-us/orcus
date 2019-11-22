91c51ebe1152f40bafdf1cdfcf2380f6e8785a8f
Kohei Yoshida <kohei.yoshida@gmail.com>
2019-11-22 15:47:42 -0500

    Add a mechanism to parse and display the results of processed files.

--- bin/process-files.py
+++ bin/process-files.py
@@ -11,6 +11,11 @@ import enum
 import orcus
 
 
+FILEEXT_GOOD = "orcus-pf.good"
+FILEEXT_BAD = "orcus-pf.bad"
+FILEEXT_OUT = "orcus-pf.out"
+
+
 def sanitize_string(s):
     """Replace non-printable characters with \\x[value]."""
 
@@ -78,33 +83,70 @@ def print_doc_preview(doc):
     return buf
 
 
+def print_results(inpath):
+    outpath = f"{inpath}.{FILEEXT_OUT}"
+    with open(outpath, "r") as f:
+        print()
+        for line in f.readlines():
+            print(f"  {line.strip()}")
+        print()
+
+
+def show_results(rootdir, good, bad):
+    for root, dir, files in os.walk(rootdir):
+        for filename in files:
+            if filename.endswith(FILEEXT_OUT) or filename.endswith(FILEEXT_GOOD) or filename.endswith(FILEEXT_BAD):
+                continue
+            inpath = os.path.join(root, filename)
+            good_filepath = f"{inpath}.{FILEEXT_GOOD}"
+            bad_filepath = f"{inpath}.{FILEEXT_BAD}"
+
+            if os.path.isfile(good_filepath) and good:
+                print(sanitize_string(inpath), flush=True)
+                print_results(inpath)
+            elif os.path.isfile(bad_filepath) and bad:
+                print(sanitize_string(inpath), flush=True)
+                print_results(inpath)
+            else:
+                continue
+
+
 def main():
     parser = argparse.ArgumentParser(
         description="""This script allows you to load a collection of input files
         for testing purpuses.""")
     parser.add_argument(
+        "--results", action="store_true", default=False,
+        help="Display the results of the processed files.")
+    parser.add_argument(
+        "--good", action="store_true", default=False,
+        help="Display the results of the successfully processed files.")
+    parser.add_argument(
+        "--bad", action="store_true", default=False,
+        help="Display the results of the unsuccessfully processed files.")
+    parser.add_argument(
         "rootdir", metavar="ROOT-DIR",
         help="Root directory below which to recursively find and process test files.")
     args = parser.parse_args()
 
-    file_count = 0
-    good_fileext = "orcus-pf.good"
-    bad_fileext = "orcus-pf.bad"
-    out_fileext = "orcus-pf.out"
+    if args.results:
+        show_results(args.rootdir, args.good, args.bad)
+        return
 
+    file_count = 0
     for root, dir, files in os.walk(args.rootdir):
         for filename in files:
-            if filename.endswith(out_fileext) or filename.endswith(good_fileext) or filename.endswith(bad_fileext):
+            if filename.endswith(FILEEXT_OUT) or filename.endswith(FILEEXT_GOOD) or filename.endswith(FILEEXT_BAD):
                 continue
 
             inpath = os.path.join(root, filename)
-            outpath = f"{inpath}.{out_fileext}"
+            outpath = f"{inpath}.{FILEEXT_OUT}"
             print(file_count, sanitize_string(inpath), flush=True)
             file_count += 1
             buf = list()
 
-            good_filepath = f"{inpath}.{good_fileext}"
-            bad_filepath = f"{inpath}.{bad_fileext}"
+            good_filepath = f"{inpath}.{FILEEXT_GOOD}"
+            bad_filepath = f"{inpath}.{FILEEXT_BAD}"
 
             if os.path.isfile(good_filepath) or os.path.isfile(bad_filepath):
                 print("already processed. skipping...")
