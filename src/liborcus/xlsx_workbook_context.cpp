e50fd76d23e2646ed6ccd6e5e39dd8fe1e558e27
Kohei Yoshida <kohei.yoshida@gmail.com>
2020-01-13 08:13:51 -0500

    Bail out on empty sheet names.

--- src/liborcus/xlsx_workbook_context.cpp
+++ src/liborcus/xlsx_workbook_context.cpp
@@ -103,6 +103,9 @@ void xlsx_workbook_context::start_element(xmlns_id_t ns, xml_token_t name, const
                 }
             );
 
+            if (sheet.name.empty())
+                throw xml_structure_error("workbook.xml: sheet element must have a valid name element.");
+
             // Insert the sheet here so that we have all the sheets available
             // prior to parsing global named expressions.
             m_factory.append_sheet(m_sheet_count++, sheet.name.data(), sheet.name.size());
