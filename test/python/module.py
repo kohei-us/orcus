7987b4590cb8d7bda08f23b1ac0a2a2a87662f35
Kohei Yoshida <kohei.yoshida@gmail.com>
2020-01-15 15:24:49 -0500

    Add test case for python's detect_format() function.

--- test/python/module.py
+++ test/python/module.py
@@ -9,12 +9,29 @@
 
 import unittest
 import orcus
+import os.path
 
 
 class ModuleTest(unittest.TestCase):
 
     def test_info(self):
         orcus.info()
+
+    def test_detect_format(self):
+        test_root_dir = os.path.join(os.path.dirname(__file__), "..")
+
+        checks = (
+            (("ods", "raw-values-1", "input.ods"), orcus.FormatType.ODS),
+            (("xlsx", "raw-values-1", "input.xlsx"), orcus.FormatType.XLSX),
+            (("xls-xml", "raw-values-1", "input.xml"), orcus.FormatType.XLS_XML),
+            (("gnumeric", "raw-values-1", "input.gnumeric"), orcus.FormatType.GNUMERIC),
+        )
+
+        for check in checks:
+            filepath = os.path.join(test_root_dir, *check[0])
+            with open(filepath, "rb") as f:
+                fmt = orcus.detect_format(f.read())
+                self.assertEqual(check[1], fmt)
 
 
 if __name__ == '__main__':
