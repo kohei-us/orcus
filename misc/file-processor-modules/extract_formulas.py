3e57e1624e431f2b5878e3f1d70dc95026558a08
Kohei Yoshida <kohei.yoshida@gmail.com>
2020-01-31 14:07:12 -0500

    Let's always output in XML. JSON is harder to read for this type of data.

--- misc/file-processor-modules/extract_formulas.py
+++ misc/file-processor-modules/extract_formulas.py
@@ -101,11 +101,6 @@ def process_document(filepath, doc):
     return output_buffer
 
 
-class OutputFormat(enum.Enum):
-    JSON = "json"
-    XML = "xml"
-
-
 def dump_json_as_xml(data, stream, add_tokens, pretty):
     import xml.etree.ElementTree as ET
     root = ET.Element("docs")
@@ -162,23 +157,16 @@ def dump_json_as_xml(data, stream, add_tokens, pretty):
 
 
 def dump_batch_to_file(i, batch, args):
-    if args.format == OutputFormat.JSON:
-        outpath = os.path.join(args.output, f"{i+1:04}.json")
-        import pprint
-        with open(outpath, "w") as f:
-            pprint.pprint(batch, stream=f, width=256)
-    elif args.format == OutputFormat.XML:
-        outpath = os.path.join(args.output, f"{i+1:04}.xml")
-        with open(outpath, "w") as f:
-            dump_json_as_xml(batch, f, args.add_tokens, args.pretty)
+    outpath = os.path.join(args.output, f"{i+1:04}.xml")
+    with open(outpath, "w") as f:
+        dump_json_as_xml(batch, f, args.add_tokens, args.pretty)
 
 
 def main():
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("-o", "--output", type=str, help="Output directory to write all the formula data to.")
-    parser.add_argument("-f", "--format", type=OutputFormat, default=OutputFormat.JSON, help="Output file format.")
-    parser.add_argument("-b", "--batch-size", type=int, default=20)
+    parser.add_argument("-b", "--batch-size", type=int, default=5)
     parser.add_argument("--add-tokens", action="store_true", default=False, help="Whether to include formula token data in the output.")
     parser.add_argument("--pretty", action="store_true", default=False, help="Whether to pretty-print the XML output.")
     parser.add_argument("rootdir", help="Root directory from which to traverse for the formula files.")
