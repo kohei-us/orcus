0d0d06207b1df25e7c855b85bd05a014f2e9f98c
hedmo <hedmoo@yahoo.com>
2020-03-28 17:25:17 +0000

    Update json_map_tree.cpp

--- src/liborcus/json_map_tree.cpp
+++ src/liborcus/json_map_tree.cpp
@@ -14,6 +14,8 @@
 
 namespace orcus {
 
+constexpr json_map_tree::child_position_type json_map_tree::node_child_default_position;
+
 namespace {
 
 void throw_path_error(const char* file, int line, const pstring& path)
