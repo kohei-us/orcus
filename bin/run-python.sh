87bc0ea3a392733780daf1e9622d8c471ea68aee
Kohei Yoshida <kohei.yoshida@gmail.com>
2020-08-13 17:28:40 -0400

    Make it so that if a file path is not given, it starts the ...
    ... interpreter in interactive mode.

--- bin/run-python.sh
+++ bin/run-python.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
+PYTHON=$(which python3)
+PROGDIR=$(dirname $0)
+_PYTHONPATH="$PROGDIR/../src/python/.libs:$PROGDIR/../src/python"
+
+export PYTHONPATH=$_PYTHONPATH:$PYTHONPATH
+export LD_LIBRARY_PATH="$PROGDIR/../src/liborcus/.libs:$PROGDIR/../src/parser/.libs"
+export DYLD_LIBRARY_PATH=$LD_LIBRARY_PATH
+
 if [ "$1" == "" ]; then
-    echo "no input file"
-    exit 1
+    # No input file. Just invoke the interpreter.
+    $PYTHON
+    exit 0
 fi
 
 RUNMODE=
@@ -19,14 +28,6 @@ if [ ! -e "$1" ]; then
     echo "file '$1' does not exist"
     exit 1
 fi
-
-PYTHON=$(which python3)
-PROGDIR=`dirname $0`
-_PYTHONPATH="$PROGDIR/../src/python/.libs:$PROGDIR/../src/python"
-
-export PYTHONPATH=$_PYTHONPATH:$PYTHONPATH
-export LD_LIBRARY_PATH="$PROGDIR/../src/liborcus/.libs:$PROGDIR/../src/parser/.libs"
-export DYLD_LIBRARY_PATH=$LD_LIBRARY_PATH
 
 EXEC="$1"
 shift
