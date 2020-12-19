d17b3990834039bf946b1631428f5db9649eb48d
Kohei Yoshida <kohei.yoshida@gmail.com>
2020-12-19 14:08:38 -0500

    Add test case for unprefixed attribute NOT belonging to default ...
    ... namespace like unprefixed elements would. For more info, see
    
    https://stackoverflow.com/questions/3312390/xml-default-namespaces-for-unqualified-attribute-names

--- src/parser/sax_ns_parser_test.cpp
+++ src/parser/sax_ns_parser_test.cpp
@@ -20,9 +20,57 @@ void test_handler()
     parser.parse();
 }
 
+/**
+ * Test for unqualified attribute NOT belonging to the default namespace,
+ * according to
+ * https://stackoverflow.com/questions/3312390/xml-default-namespaces-for-unqualified-attribute-names
+ */
+void test_default_attr_ns()
+{
+    const orcus::xmlns_id_t default_ns = "test:foo";
+
+    struct _handler : public orcus::sax_ns_handler
+    {
+        orcus::xmlns_id_t default_ns_expected;
+
+        void start_element(const orcus::sax_ns_parser_element& elem)
+        {
+            // All elements should belong to the default namespace.
+            assert(elem.ns == default_ns_expected);
+        }
+
+        void attribute(const orcus::pstring& /*name*/, const orcus::pstring& /*val*/) {}
+
+        void attribute(const orcus::sax_ns_parser_attribute& attr)
+        {
+            // Attribute's namespace should be empty.
+            assert(attr.ns == orcus::XMLNS_UNKNOWN_ID);
+            assert(attr.name == "attr");
+            assert(attr.value == "1");
+        }
+    };
+
+    const char* test_code = "<?xml version=\"1.0\"?><root xmlns='test:foo'><elem attr='1'/></root>";
+    size_t len = strlen(test_code);
+
+    const orcus::xmlns_id_t predefined[] = { default_ns, nullptr };
+
+    orcus::xmlns_repository repo;
+    repo.add_predefined_values(predefined);
+
+    orcus::xmlns_context cxt = repo.create_context();
+
+    _handler hdl;
+    hdl.default_ns_expected = default_ns;
+
+    orcus::sax_ns_parser<_handler> parser(test_code, len, cxt, hdl);
+    parser.parse();
+}
+
 int main()
 {
     test_handler();
+    test_default_attr_ns();
 
     return EXIT_SUCCESS;
 }
