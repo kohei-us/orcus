ab0376681244ddbe6cdaf23350adf3f93e0ec359
Kohei Yoshida <kohei.yoshida@gmail.com>
2021-12-03 19:43:10 -0500

    Add xml_empty_context to use it to ignore XML sub structures.

---
+++ src/liborcus/xml_empty_context.cpp
@@ -0,0 +1,39 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "xml_empty_context.hpp"
+
+namespace orcus {
+
+xml_context_base* xml_empty_context::create_child_context(xmlns_id_t /*ns*/, xml_token_t /*name*/)
+{
+    return nullptr;
+}
+
+void xml_empty_context::end_child_context(
+    xmlns_id_t /*ns*/, xml_token_t /*name*/, xml_context_base* /*child*/)
+{
+}
+
+void xml_empty_context::start_element(
+    xmlns_id_t ns, xml_token_t name, const::std::vector<xml_token_attr_t>& /*attrs*/)
+{
+    push_stack(ns, name);
+}
+
+bool xml_empty_context::end_element(xmlns_id_t ns, xml_token_t name)
+{
+    return pop_stack(ns, name);
+}
+
+void xml_empty_context::characters(std::string_view /*str*/, bool /*transient*/)
+{
+}
+
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
---
+++ src/liborcus/xml_empty_context.hpp
@@ -0,0 +1,37 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#pragma once
+
+#include "xml_context_base.hpp"
+
+namespace orcus {
+
+/**
+ * This context only tracks the scopes of all encountered elements but does
+ * nothing else.  It is to be used when you need to ignore a whole sub
+ * structure of an XML document.
+ */
+class xml_empty_context : public xml_context_base
+{
+public:
+    virtual xml_context_base* create_child_context(xmlns_id_t ns, xml_token_t name) override;
+
+    virtual void end_child_context(
+        xmlns_id_t ns, xml_token_t name, xml_context_base* child) override;
+
+    virtual void start_element(
+        xmlns_id_t ns, xml_token_t name, const std::vector<xml_token_attr_t>& attrs) override;
+
+    virtual bool end_element(xmlns_id_t ns, xml_token_t name) override;
+
+    virtual void characters(std::string_view str, bool transient) override;
+};
+
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
