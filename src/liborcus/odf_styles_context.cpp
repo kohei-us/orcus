6854616b4cb1243c3dc900e79aa7849c4f3bf531
Kohei Yoshida <kohei.yoshida@gmail.com>
2022-06-07 20:07:29 -0400

    Let's use insert_or_assign from C++17

--- src/liborcus/odf_styles_context.cpp
+++ src/liborcus/odf_styles_context.cpp
@@ -580,50 +580,44 @@ void styles_context::start_table_cell_properties(const xml_token_pair_t& parent,
                     };
 
                     for (const auto dir : dirs)
-                        border_styles.insert(border_map_type::value_type(dir, border_details));
+                        border_styles.insert_or_assign(dir, border_details);
 
                     break;
                 }
                 case XML_border_top:
                 {
                     odf::border_details_t border_details = odf::extract_border_details(attr.value);
-                    border_styles.insert(
-                        border_map_type::value_type(ss::border_direction_t::top, border_details));
+                    border_styles.insert_or_assign(ss::border_direction_t::top, border_details);
                     break;
                 }
                 case XML_border_bottom:
                 {
                     odf::border_details_t border_details = odf::extract_border_details(attr.value);
-                    border_styles.insert(
-                        border_map_type::value_type(ss::border_direction_t::bottom, border_details));
+                    border_styles.insert_or_assign(ss::border_direction_t::bottom, border_details);
                     break;
                 }
                 case XML_border_left:
                 {
                     odf::border_details_t border_details = odf::extract_border_details(attr.value);
-                    border_styles.insert(
-                        border_map_type::value_type(ss::border_direction_t::left, border_details));
+                    border_styles.insert_or_assign(ss::border_direction_t::left, border_details);
                     break;
                 }
                 case XML_border_right:
                 {
                     odf::border_details_t border_details = odf::extract_border_details(attr.value);
-                    border_styles.insert(
-                        border_map_type::value_type(ss::border_direction_t::right, border_details));
+                    border_styles.insert_or_assign(ss::border_direction_t::right, border_details);
                     break;
                 }
                 case XML_diagonal_bl_tr:
                 {
                     odf::border_details_t border_details = odf::extract_border_details(attr.value);
-                    border_styles.insert(
-                        border_map_type::value_type(ss::border_direction_t::diagonal_bl_tr, border_details));
+                    border_styles.insert_or_assign(ss::border_direction_t::diagonal_bl_tr, border_details);
                     break;
                 }
                 case XML_diagonal_tl_br:
                 {
                     odf::border_details_t border_details = odf::extract_border_details(attr.value);
-                    border_styles.insert(
-                        border_map_type::value_type(ss::border_direction_t::diagonal_tl_br, border_details));
+                    border_styles.insert_or_assign(ss::border_direction_t::diagonal_tl_br, border_details);
                     break;
                 }
                 default:
