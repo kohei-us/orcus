0a32f053a67c0a3adfc3e259e79142d95b7d9a66
Kohei Yoshida <kohei.yoshida@gmail.com>
2022-08-17 19:00:36 -0400

    Use autoreconf over autoconf

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-TESTLIBTOOLIZE="glibtoolize libtoolize"
-
-LIBTOOLIZEFOUND="0"
 
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
@@ -9,59 +6,11 @@ test -z "$srcdir" && srcdir=.
 olddir=`pwd`
 cd $srcdir
 
-aclocal --version > /dev/null 2> /dev/null || {
-    echo "error: aclocal not found"
-    exit 1
-}
-automake --version > /dev/null 2> /dev/null || {
-    echo "error: automake not found"
+autoreconf -i || {
+    echo "error: autoreconf failed"
     exit 1
 }
 
-for i in $TESTLIBTOOLIZE; do
-	if which $i > /dev/null 2>&1; then
-		LIBTOOLIZE=$i
-		LIBTOOLIZEFOUND="1"
-		break
-	fi
-done
-
-if [ "$LIBTOOLIZEFOUND" = "0" ]; then
-	echo "$0: need libtoolize tool to build orcus" >&2
-	exit 1
+if test x$NOCONFIGURE = x; then
+    ./configure $@
 fi
-
-amcheck=`automake --version | grep 'automake (GNU automake) 1.5'`
-if test "x$amcheck" = "xautomake (GNU automake) 1.5"; then
-    echo "warning: you appear to be using automake 1.5"
-    echo "         this version has a bug - GNUmakefile.am dependencies are not generated"
-fi
-
-rm -rf autom4te*.cache
-
-$LIBTOOLIZE --force --copy || {
-    echo "error: libtoolize failed"
-    exit 1
-}
-aclocal -I m4 $ACLOCAL_FLAGS || {
-    echo "error: aclocal $ACLOCAL_FLAGS failed"
-    exit 1
-}
-autoheader || {
-    echo "error: autoheader failed"
-    exit 1
-}
-automake -a -c --foreign || {
-    echo "warning: automake failed"
-}
-automake --add-missing || {
-    echo "error: failed to add missing stuff"
-    exit 1
-}
-autoconf || {
-    echo "error: autoconf failed"
-    exit 1
-}
-
-test x$NOCONFIGURE = x && ./configure $@
-
