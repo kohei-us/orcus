b3c5700adf790cdabce40bd1a94d512b68049189
Kohei Yoshida <kohei.yoshida@gmail.com>
2023-05-18 21:02:28 -0400

    Use column descriptor to determine column value types

--- misc/arrow-parquet/arrow-test.cpp
+++ misc/arrow-parquet/arrow-test.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <memory>
 #include <string>
+#include <sstream>
 
 using std::cout;
 using std::cerr;
@@ -57,10 +58,15 @@ int main(int argc, char** argv)
     cout << "  name: " << p->name() << endl;
     cout << "  num-columns: " << p->num_columns() << endl;
 
+    std::vector<const parquet::ColumnDescriptor*> column_types;
+    column_types.reserve(p->num_columns());
+
     for (int i = 0; i < p->num_columns(); ++i)
     {
         cout << "column " << i << ":" << endl;
         const parquet::ColumnDescriptor* col_desc = p->Column(i);
+        column_types.push_back(col_desc);
+
         cout << "  name: " << col_desc->name() << endl;
         cout << "  physical type: " << col_desc->physical_type() << endl;
         cout << "  converted type: " << col_desc->converted_type() << endl;
@@ -69,18 +75,75 @@ int main(int argc, char** argv)
 
     parquet::StreamReader stream{std::move(file_reader)};
 
-    std::string c1, c2, c4;
-    bool c3;
+    if (stream.eof())
+        return EXIT_SUCCESS;
 
     cout << "row values:" << endl;
 
-    if (!stream.eof())
+    // print column labels
+    for (const parquet::ColumnDescriptor* p : column_types)
+        cout << p->name() << ' ';
+    cout << endl;
+
+    for (int i = 0; i < r.num_rows(); ++i)
     {
-        for (int i = 0; i < r.num_rows(); ++i)
+        for (const parquet::ColumnDescriptor* p : column_types)
         {
-            stream >> c1 >> c2 >> c3 >> c4 >> parquet::EndRow;
-            cout << c1 << ' ' << c2 << ' ' << c3 << ' ' << c4 <<    endl;
+            switch (p->physical_type())
+            {
+                case parquet::Type::BYTE_ARRAY:
+                {
+                    switch (p->converted_type())
+                    {
+                        case parquet::ConvertedType::UTF8:
+                        {
+                            std::string v;
+                            stream >> v;
+                            cout << v << ' ';
+                            break;
+                        }
+                        default:
+                            throw std::runtime_error("WIP: unhandled converted type for BYTE_ARRAY");
+                    }
+                    break;
+                }
+                case parquet::Type::INT64:
+                {
+                    switch (p->converted_type())
+                    {
+                        case parquet::ConvertedType::NONE:
+                        {
+                            int64_t v;
+                            stream >> v;
+                            cout << v << ' ';
+                            break;
+                        }
+                        default:
+                            throw std::runtime_error("WIP: unhandled converted type for INT64");
+                    }
+                    break;
+                }
+                case parquet::Type::BOOLEAN:
+                {
+                    if (p->converted_type() != parquet::ConvertedType::NONE)
+                        throw std::runtime_error("WIP: unhandled covnerted type for BOOLEAN");
+
+                    bool v;
+                    stream >> v;
+                    cout << v << ' ';
+                    break;
+                }
+                default:
+                {
+                    std::ostringstream os;
+                    os << "WIP: not handled type: physical=" << p->physical_type() << "; converted=" << p->converted_type();
+                    throw std::runtime_error(os.str());
+                }
+            }
         }
+
+        stream >> parquet::EndRow;
+        cout << endl;
     }
 
     return EXIT_SUCCESS;
--- misc/arrow-parquet/gen-parquet.py
+++ misc/arrow-parquet/gen-parquet.py
@@ -6,10 +6,11 @@ import pyarrow as pa
 
 
 def main():
-    df = pd.DataFrame({'one': ["andy", "bruce", "charlie", "david"],
-                       'two': ['foo', 'bar', 'baz', "hmm"],
-                       'three': [True, False, True, True]},
-                       index=list('abcd'))
+    df = pd.DataFrame({'one': ["andy", "bruce", "charlie", "david", "edward"],
+                       'two': ['foo', 'bar', 'baz', "hmm", "more"],
+                       'three': [True, False, True, True, False],
+                       'four': [11, 22, 33, 44, 55]},
+                       index=list('abcde'))
 
     print(df)
     table = pa.Table.from_pandas(df)
