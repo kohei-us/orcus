b583e7f1d6a13d26bef8881a12b3c6aaf7dc787a
Kohei Yoshida <kohei.yoshida@gmail.com>
2023-07-07 22:21:44 -0400

    Use boost::filesystem & clean things up a bit

--- src/orcus_test_gnumeric.cpp
+++ src/orcus_test_gnumeric.cpp
@@ -6,53 +6,53 @@
  */
 
 #include "orcus_test_global.hpp"
-#include "orcus/orcus_gnumeric.hpp"
-#include "orcus/stream.hpp"
-#include "orcus/spreadsheet/factory.hpp"
-#include "orcus/spreadsheet/document.hpp"
 
+#include <orcus/orcus_gnumeric.hpp>
+#include <orcus/stream.hpp>
+#include <orcus/spreadsheet/factory.hpp>
+#include <orcus/spreadsheet/document.hpp>
+
+#include <boost/filesystem/path.hpp>
 #include <iostream>
-
-#include <mdds/flat_segment_tree.hpp>
+#include <sstream>
 
 using namespace orcus;
-using namespace std;
+namespace fs = boost::filesystem;
 
 namespace {
 
-std::vector<const char*> dirs = {
+std::vector<fs::path> dirs = {
     SRCDIR"/test/gnumeric/raw-values-1/",
     SRCDIR"/test/gnumeric/formula-cells/",
 };
 
 void test_gnumeric_import()
 {
-    for (const char* dir : dirs)
-    {
-        string path(dir);
+    ORCUS_TEST_FUNC_SCOPE;
 
-        std::cout << "checking " << path << "..." << std::endl;
+    for (const auto& dir : dirs)
+    {
+        std::cout << "checking " << dir << "..." << std::endl;
 
         // Read the input.gnumeric document.
-        path.append("input.gnumeric");
+        fs::path filepath = dir / "input.gnumeric";
         spreadsheet::range_size_t ss{1048576, 16384};
         spreadsheet::document doc{ss};
         spreadsheet::import_factory factory(doc);
         orcus_gnumeric app(&factory);
-        app.read_file(path.c_str());
+        app.read_file(filepath.string());
 
         // Gnumeric doc doesn't cache formula results.
         doc.recalc_formula_cells();
 
         // Dump the content of the model.
-        ostringstream os;
+        std::ostringstream os;
         doc.dump_check(os);
-        string check = os.str();
+        std::string check = os.str();
 
         // Check that against known control.
-        path = dir;
-        path.append("check.txt");
-        file_content control(path.data());
+        filepath = dir / "check.txt";
+        file_content control(filepath.string());
 
         assert(!check.empty());
         assert(!control.empty());
