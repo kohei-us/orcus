e1528b856c1f085279c5365720fb99e389b865c7
Kohei Yoshida <kohei.yoshida@gmail.com>
2024-05-15 21:45:52 -0400

    Test the strikethrough handling in format runs in xlsx

--- src/orcus_xlsx_test.cpp
+++ src/orcus_xlsx_test.cpp
@@ -58,6 +58,34 @@ std::unique_ptr<ss::document> load_doc(const std::string_view& path, bool recalc
 }
 
 /**
+ * Check if we can consider the strikethrough attribute to be "set" given the
+ * mapping we do between Excel's attribute, which is just boolean,
+ * and how we internally store, which consists of 4 separate attributes.
+ */
+bool strikethrough_set(const ss::strikethrough_t& st)
+{
+    if (!st.style)
+        return false;
+
+    if (*st.style != ss::strikethrough_style_t::solid)
+        return false;
+
+    if (!st.type)
+        return false;
+
+    if (*st.type != ss::strikethrough_type_t::single_type)
+        return false;
+
+    if (!st.width)
+        return false;
+
+    if (*st.width != ss::strikethrough_width_t::width_auto)
+        return false;
+
+    return true;
+}
+
+/**
  * Convenience function to retrieve a pivot cache instance from textural
  * sheet name and range name.
  */
@@ -1344,18 +1372,22 @@ void test_xlsx_formatted_text_basic()
         row = 9;
         assert(check_cell_text(*sheet, row, col, "All Strikethrough"));
         font = get_font(*sheet, row, col);
-        assert(font->strikethrough.style);
-        assert(*font->strikethrough.style == ss::strikethrough_style_t::solid);
+        assert(strikethrough_set(font->strikethrough));
 
-        assert(font->strikethrough.type);
-        assert(*font->strikethrough.type == ss::strikethrough_type_t::single_type);
-
-        assert(font->strikethrough.width);
-        assert(*font->strikethrough.width == ss::strikethrough_width_t::width_auto);
-
-        // A11:A13 - TODO: check format
+        // A11
         row = 10;
         assert(check_cell_text(*sheet, row, col, "Partial strikethrough"));
+        si = sheet->get_string_identifier(row, col);
+        runs = doc->get_shared_strings().get_format_runs(si);
+        assert(runs);
+        assert(runs->size() == 1u);
+        // Partial strikethrough
+        //         ^^^^^^^^^^^^^
+        assert(runs->at(0).pos == 8);
+        assert(runs->at(0).size == 13);
+        assert(strikethrough_set(runs->at(0).strikethrough));
+
+        // A12:A13 - TODO: check format
         row = 11;
         assert(check_cell_text(*sheet, row, col, "Superscript"));
         row = 12;
