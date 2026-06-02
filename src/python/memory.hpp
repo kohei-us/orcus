afa8eb799b24fcc31d44b590eec9740c4a53de58
Kohei Yoshida <kohei.yoshida@collabora.com>
2026-06-01 21:59:14 -0400

    Add usage guidelines for py_scoped_ref (#239)

--- src/python/memory.hpp
+++ src/python/memory.hpp
@@ -19,6 +19,21 @@ struct pyobj_unique_deleter
 
 using py_unique_ptr = std::unique_ptr<PyObject, pyobj_unique_deleter>;
 
+/**
+ * RAII that owns one PyObject reference.
+ *
+ * When the CPython API doc says:
+ *
+ * <li><em>new reference</em> (e.g. PyTuple_New, tp_new): assign to a new
+ * instance of py_scoped_ref directly.</li>
+ * <li><em>borrowed reference</em> (e.g. PyTuple_GetItem): don't wrap it
+ * in py_scoped_ref</li>
+ * <li><em>steals</em> (e.g. PyTuple_SetItem): call release() to surrender
+ *     ownership; prefer to call set_tuple_item_new().</li>
+ * <li>non-stealing store that increases ref count (e.g. PyDict_SetItem,
+ * PySet_Add): pass get(); prefer to call set_dict_item_new() or
+ * add_to_set_new().</li>
+ */
 class py_scoped_ref
 {
     PyObject* m_pyobj;
